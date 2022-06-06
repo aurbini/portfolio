98c2b92880b82b13e662cc76ffb466bd4abb6e3f
alexander urbini <alex@alexanders-MBP.lan>
2022-06-06 18:26:38 -0400

    centered carousel, nav show/hide on scroll

--- scripts.js
+++ scripts.js
@@ -1,12 +1,16 @@
 var lastScrollTop;
 navbar = document.getElementById('navbar');
 window.addEventListener('scroll', function () {
-  console.log('object');
   var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
   if (scrollTop > lastScrollTop) {
     navbar.style.top = '-80px';
+    navbar.classList.remove('scroll-nav');
   } else {
     navbar.style.top = '0';
+    navbar.classList.add('scroll-nav');
+  }
+  if (this.window.pageYOffset == 0) {
+    navbar.classList.remove('scroll-nav');
   }
   lastScrollTop = scrollTop;
 });
